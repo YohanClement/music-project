2c78e7dd31b8afc24077dde2eea52a4a74a0d58e
Lokionn <122351015+Lokionn@users.noreply.github.com>
2023-02-08 12:52:28 +0100

    La création d'évenement
    1. Les views : 
    Ds la liste user j'ai rajouté un lien qui  permet de voir la liste des évènement du user connecté sous forme de tableau 
    La page creaevent qui permet de creer levenement
    La page Event created successfully ou on arrive quand on créé l'event 
    2. Les entités : 
    j'ai modifié user (rajouté un to string) 
    j'ai modifié event (ce que je vous ai dis int -> integer et enlever la classe chelou eventID)
    3. Les repository : 
    Creation du UserEventRepository qui permet la mise a jour de la table d'association a la création d'un évènement 
    utilisation de findAllByUsers : permet de trouver tous les evenement ou un user est lié 
    Utilisation du EvenementRepository pour sauvegarder l'evenement 
    4. Le controller : 
    Get crea event -> on arrive sur la page de création 
    Post crea : gestion des champs de dates debut et date fin (vous remarquerez que ds le formulaire les dates n'ont pas le nom de la colonne : l'attribution se fait coté controller APRES cast sur un type adapté) 
    On gere aussi ds le GetPost la mise a jour de la table d'association 
    GetList : je recupere le user actif et la liste de ses evenement pour les afficher 
    5. MySQL : mettez a jour les bases suivantes : 
    -evenement : date -> datedebut et datefin tous deux non nuls 
    -evenement : création de colonne evenement lieu : non nulle
    -evenement : groupe crea etait non nul : laissez le etre nul 
    -user evenement : creation de la colonne user_Eventkey non null AI PK

--- AppController.java
+++ AppController.java
@@ -1,7 +1,8 @@
 package fr.formation.inti.controller;
 
 import java.io.IOException;
-import java.security.Principal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,33 +21,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import fr.formation.inti.config.FileUploadUtil;
-import fr.formation.inti.model.UserRoles;
-import fr.formation.inti.model.Users;
-import fr.formation.inti.model.UsersInstru;
-import fr.formation.inti.repository.InstrumentsDAO;
-import fr.formation.inti.repository.UserRolesDAO;
-import fr.formation.inti.repository.UsersDAO;
+import fr.formation.inti.entity.Evenement;
+import fr.formation.inti.entity.GenreMusic;
+import fr.formation.inti.entity.UserRoles;
+import fr.formation.inti.entity.Users;
+import fr.formation.inti.entity.UsersEvenement;
+import fr.formation.inti.entity.UsersInstruments;
+import fr.formation.inti.repository.EvenementRepository;
+import fr.formation.inti.repository.GenreMusicRepository;
+import fr.formation.inti.repository.MusicInstrumentRepository;
+import fr.formation.inti.repository.UserEventRepository;
+import fr.formation.inti.repository.UserRepository;
+import fr.formation.inti.repository.UserRoleRepository;
 import fr.formation.inti.repository.UsersInstruDAO;
 
 @Controller
 public class AppController {
-
+	
 	@Autowired
-	private UsersDAO userRepo;
+	private UserRepository userRepo;
+	
 	@Autowired
-	private UserRolesDAO rolesRepo;
+	private MusicInstrumentRepository instrRepo;
+	
 	@Autowired
-	private InstrumentsDAO instrRepo;
+	private UserRoleRepository rolesRepo;
+	
 	@Autowired
 	private UsersInstruDAO uInstrRepo;
 	
+	@Autowired
+	private EvenementRepository eventRepo;
+	
+	@Autowired
+	private GenreMusicRepository genreMusRepo;
+	
+	@Autowired
+	private UserEventRepository uEventRepo;
+
 	private boolean isAuthenticated() {
 	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 	    if (authentication == null || AnonymousAuthenticationToken.class.
 	      isAssignableFrom(authentication.getClass())) {
+	    	System.out.println("authentication : "+authentication);
+	    	System.out.println("autre truc long : "+AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass()));
 	        return false;
 	    }
+	    System.out.println(userRepo);
 	    return authentication.isAuthenticated();
 	}
 	
@@ -67,13 +89,6 @@ public class AppController {
 	    return "creationprofil";
 	}
 	
-	@GetMapping("/connexion")
-	public String showlogForm(Model model) {
-	    model.addAttribute("user", new Users());
-	     
-	    return "Connexion";
-	}
-	
 	@GetMapping("/listusers")
 	public String listUsers(Model model) {
 	    List<Users> listUsers = userRepo.findAll();
@@ -86,7 +101,7 @@ public class AppController {
 	}
 	
 	@PostMapping("/process_register")
-	public String processRegister(Users user,@RequestParam("instruments") List<String> instruments, @RequestParam("niveau") Integer niveau, @RequestParam("image") MultipartFile multipartFile) throws IOException {
+	public String processRegister(Users user,@RequestParam("instruments") String instruments, @RequestParam("niveau") Integer niveau, @RequestParam("image") MultipartFile multipartFile) throws IOException {
 		System.out.println(user);
 	    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	    String encodedPassword = passwordEncoder.encode(user.getPassword());
@@ -94,19 +109,19 @@ public class AppController {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
 		// System.out.println(formatter.format(date));
-		user.setDatecrea(now);
-		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		user.setUsersDateCrea(now);
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());	
         user.setPhotos(fileName);
         
 	    Users savedUser =userRepo.save(user);
-	    UserRoles userrole =new UserRoles(user, "USER", user.getEmailaddress());
+	    UserRoles userrole =new UserRoles(user, "USER", user.getUsersEmail());
 	    rolesRepo.save(userrole);
 	    
 	    
-	    UsersInstru uInst = new UsersInstru(savedUser, instrRepo.findByinstrname(instruments.get(0)), niveau);
-	    uInstrRepo.save(uInst);
-	    
-	    String uploadDir = "user-photos/" + savedUser.getUsersid();
+	    UsersInstruments uInst = new UsersInstruments(savedUser, instrRepo.findByinstrName(instruments), niveau);
+	    UsersInstruments uInstsaved = uInstrRepo.save(uInst);
+	    System.out.println(uInstsaved.getUserInstrumentId());
+	    String uploadDir = "user-photos/" + savedUser.getUsersId();
 	    
         FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 	    return "register_success";
@@ -119,16 +134,104 @@ public class AppController {
 		return "logoutSuccessful";
 	}
 	
+//	public static void main(String[] args) {
+//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		
+//		String password="Abc123!";
+//		String encodePw = encoder.encode(password);
+//		System.out.println(encodePw);
+//		System.out.println(encodePw);
+//	}
 
-	@GetMapping(value = "/403")
-	public String accessDenied(Model model, Principal principal) {
-
-		if (principal != null) {
-			model.addAttribute("message",
-					"Hi " + principal.getName() + "<br> You do not have permission to access this page!");
-		} else {
-			model.addAttribute("msg", "You do not have permission to access this page!");
-		}
-		return "403Page";
+	
+	
+	@GetMapping("/creaevent")
+	public String CreaEvent(Model model) {
+	   
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    System.out.println(auth.getName());
+	    model.addAttribute("user", new Users());
+	    if (isAuthenticated()) {
+	    	return "CreaEvent";
+	    }
+	    return "login";
 	}
+	
+	
+	@PostMapping("/process_creaevent")
+	public String processCreaEvent(Evenement event,@RequestParam("evenementDatedebut1") String datedebut, 
+			@RequestParam("evenementDatefin1") String datefin,@RequestParam("genreMusic1") String genremusic) throws IOException {
+		System.out.println("tostring1 :  " + event.toString());
+		
+		
+	
+		System.out.println(datedebut);
+		System.out.println(datefin);
+		
+	//	DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"); //H majuscule pour etre en format 24h
+		
+		LocalDateTime debut = LocalDateTime.parse(datedebut, dtf); 
+		LocalDateTime fin = LocalDateTime.parse(datefin, dtf); 
+		System.out.println("marqueur 2 : " + debut);
+		System.out.println("marqueur 3 : " + fin);
+		
+		event.setEvenementDatedebut(debut);
+		event.setEvenementDatefin(fin);
+		
+		GenreMusic genremusic1 = genreMusRepo.findByGenreName(genremusic);
+		
+		event.setGenreMusic(genremusic1);
+
+		System.out.println("tostring2 :  " + event.toString());
+
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+		  String username = ((UserDetails)principal).getUsername();
+		  System.out.println("case 1 : " + username);
+		  Users activeuser = userRepo.findByEmail(username);
+		  event.setUsers(activeuser);
+		} else {
+		  String username = principal.toString();
+		  System.out.println("case 2 : " + username);
+
+		}
+		//Il faut que le user qui créé l'event soit enregistré par défaut ds la colonne user crea
+		// il faut aussi mettre à jour la table d'association user-event en y ajoutant le couple createur -event
+		//ensuite il faudra gerer les invitations via la page d'evenement, un "je participe" mets a jour la table event -user
+		
+		String username = ((UserDetails)principal).getUsername();
+		Users activeuser = userRepo.findByEmail(username);
+		System.out.println("user actif id : " + activeuser.getUsersId());
+		
+//	    Users savedUser =userRepo.save(user);
+		Evenement savedEvent = eventRepo.save(event);
+		System.out.println("saved event id : " + savedEvent.getEvenementId());
+		System.out.println("saved event  : " + savedEvent.getEvenementId());
+		System.out.println("saved event id 2: " + event.getEvenementId());
+		
+		
+		
+		UsersEvenement uEvent = new UsersEvenement(savedEvent,activeuser);
+		UsersEvenement saveduEvent = uEventRepo.save(uEvent);
+		System.out.println(saveduEvent.toString());
+	    return "event_created_successfully";
+	}
+	
+	@GetMapping("/listeevent")
+	public String listEvent(Model model) {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = ((UserDetails)principal).getUsername();
+		Users activeuser = userRepo.findByEmail(username);
+		
+	    List<UsersEvenement> listuEvenement = uEventRepo.findAllByUsers(activeuser);
+	    model.addAttribute("listuEvent", listuEvenement);
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    System.out.println(auth.getName());
+	    Users user = userRepo.findByEmail(auth.getName());
+	    model.addAttribute("user", user);
+	    return "listevent";
+	}
+	
 }
